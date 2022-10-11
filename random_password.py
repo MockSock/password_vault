a8789b808756b0e2a4b4b9f7c571f5f138f239fe
madden20202 <business.mocksock@gmail.com>
2022-10-11 13:48:14 -0500

    start of a password generator

---
+++ random_password.py
@@ -0,0 +1,10 @@
+import secrets, string 
+
+# All characters
+
+letters = string.ascii_letters
+numbers = string.digits
+special_characters = string.punctuation
+
+# Connect the characters together 
+alphabet = letters + numbers + special_characters 
