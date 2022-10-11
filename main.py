7842acbe4ae05e2fed3a1d5df55f4295bdd488fe
madden20202 <business.mocksock@gmail.com>
2022-10-11 18:42:09 -0500

    create basic work for rest api

--- main.py
+++ main.py
@@ -4,4 +4,18 @@
 # Create a login for user 
 # Find a way to make a better constraint for the password maker
 
+import string
 from flask import Flask
+
+user = ''
+# common acryonym for password
+pwd = ''
+
+app = Flask(__name__)
+
+@app.route('/')
+def test_call():
+    return ('Test 123')
+
+if __name__ == '__main__':
+    app.run()
