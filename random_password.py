b4c6c26972f5470297d4a57ff7b68ad37f281f8d
madden20202 <business.mocksock@gmail.com>
2022-10-11 14:04:47 -0500

    finish up making the core logic

--- random_password.py
+++ random_password.py
@@ -8,3 +8,14 @@ special_characters = string.punctuation
 
 # Connect the characters together 
 alphabet = letters + numbers + special_characters 
+
+# Set a password length 
+password_length = 18
+
+# Now to generate the password
+password = ''
+
+for i in range(password_length): 
+    password += ''.join(secrets.choice(alphabet))
+
+print(password)
