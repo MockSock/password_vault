e42d205fe831a6ed12090dbdbb79b0b3b9b154ff
madden20202 <business.mocksock@gmail.com>
2022-10-11 14:29:47 -0500

    add a password with constraints

--- random_password.py
+++ random_password.py
@@ -18,4 +18,11 @@ password = ''
 for i in range(password_length): 
     password += ''.join(secrets.choice(alphabet))
 
+# Password with Constraints
+while True: 
+    for i in range(password_length):
+        password += ''.join(secrets.choice(alphabet))
+
+    if (any(char in special_characters for char in password) and sum(char in numbers for char in password) >=2 ): break
+
 print(password)
