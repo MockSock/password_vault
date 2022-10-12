8358ffe9ac20fd2134b7e4b0049aa4e1a9752719
madden20202 <business.mocksock@gmail.com>
2022-10-11 19:04:32 -0500

    add def for making passwords

--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 # Create a login for user 
 # Find a way to make a better constraint for the password maker
 
-import string
 from flask import Flask, render_template
+
+from random_password import password
 
 user = ''
 # common acryonym for password
@@ -17,6 +18,10 @@ app = Flask(__name__)
 def test_call():
     return render_template('index.html')
     
+# Will create a random password
+@app.route('/createpwd')
+def create_pwd():
+    return password
 
 if __name__ == '__main__':
     app.run()
