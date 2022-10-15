1ad57bd4b8eb58d0151ef10b0534b4e89fb28974
madden20202 <business.mocksock@gmail.com>
2022-10-15 16:52:00 -0500

    create password variable and a filekey

--- src/security.py
+++ src/security.py
@@ -5,7 +5,9 @@ from cryptography.fernet import Fernet
 from random_password import password
 
 # get password from user 
+storagePwd = password
 # for now it'll be randomly generated
+fileKey = Fernet.generate_key()
 
 # Attack key to file for storage 
 # Most likely will make a dictionary for a user that will have multiple passwords and sites
