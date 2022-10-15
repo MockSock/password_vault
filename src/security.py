67d3a8cda2c0c1692b2b022a19dfede4511d53ad
madden20202 <business.mocksock@gmail.com>
2022-10-15 16:45:33 -0500

    add import and notes

--- src/security.py
+++ src/security.py
@@ -1,1 +1,10 @@
 # this file will handle encryption, decryption, and tokenization
+
+from cryptography.fernet import Fernet
+
+# get password from user 
+# for now it'll be randomly generated
+
+# Attack key to file for storage 
+# Most likely will make a dictionary for a user that will have multiple passwords and sites
+
