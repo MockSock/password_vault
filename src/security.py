7994d4b76734178a89c2f17dea03ac78f70d45a6
madden20202 <business.mocksock@gmail.com>
2022-10-16 13:12:32 -0500

    enwrap encryption work in a class

--- src/security.py
+++ src/security.py
@@ -5,20 +5,23 @@ from cryptography.fernet import Fernet
 fileKey = Fernet.generate_key()
 fernet = Fernet(fileKey)
 
-# Attack key to file for storage 
-# Most likely will make a dictionary for a user that will have multiple passwords and sites
-def saveFileKey():
-    with open('user.key', 'wb') as filekey:
-        filekey.write(fileKey)
+class Encrytion:
 
-# Open and use key 
-with open('user.key', 'rb') as file:
-    original = file.read()
+    # Attack key to file for storage 
+    # Most likely will make a dictionary for a user that will have multiple passwords and sites
+    def saveFileKey():
+        with open('user.key', 'wb') as filekey:
+            filekey.write(fileKey)
 
-# Encrypt file 
-encryptAction = fernet.encrypt(original)
+    # Open and use key 
+    with open('user.key', 'rb') as file:
+        original = file.read()
 
-# Write password to file 
-def savePwd(storagePwd):
-    with open('user_pwd.txt', 'wb') as encrypted_file:
-        encrypted_file.write(storagePwd)
+    # Encrypt file 
+    encryptAction = fernet.encrypt(original)
+
+    # Write password to file 
+    def savePwd(storagePwd):
+        with open('user.key', 'wb') as encrypted_file:
+            encrypted_file.write(storagePwd) 
+
