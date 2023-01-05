a31822ce1e31474f02fc3eec0b239a5b689b2cf5
madden20202 <business.mocksock@gmail.com>
2023-01-05 16:38:05 -0600

    wrap eveerything into a class

--- src/random_password.py
+++ src/random_password.py
@@ -1,28 +1,29 @@
 import secrets, string 
 
-# All characters
+class Password_Generator():
+    # All characters
 
-letters = string.ascii_letters
-numbers = string.digits
-special_characters = string.punctuation
+    letters = string.ascii_letters
+    numbers = string.digits
+    special_characters = string.punctuation
 
-# Connect the characters together 
-alphabet = letters + numbers + special_characters 
+    # Connect the characters together 
+    alphabet = letters + numbers + special_characters 
 
-# Set a password length 
-password_length = 18
+    # Set a password length 
+    password_length = 18
 
-# Now to generate the password
-password = ''
+    # Now to generate the password
+    password = ''
 
-for i in range(password_length): 
-    password += ''.join(secrets.choice(alphabet))
+    for i in range(password_length): 
+        password += ''.join(secrets.choice(alphabet))
 
-# Password with Constraints
-# while True: 
-#     for i in range(password_length):
-#         password += ''.join(secrets.choice(alphabet))
+    # Password with Constraints
+    # while True: 
+    #     for i in range(password_length):
+    #         password += ''.join(secrets.choice(alphabet))
 
-#     if (any(char in special_characters for char in password) and sum(char in numbers for char in password) >=2 ): break
+    #     if (any(char in special_characters for char in password) and sum(char in numbers for char in password) >=2 ): break
 
-print(password)
+    print(password)
