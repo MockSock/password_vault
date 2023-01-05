1b653d2dce4db06f667918c3082cb6809626b3d9
madden20202 <business.mocksock@gmail.com>
2023-01-05 16:49:06 -0600

    experimental rework of password generator

--- src/random_password.py
+++ src/random_password.py
@@ -1,23 +1,24 @@
 import secrets, string 
 
-class Password_Generator():
-    # All characters
-
-    letters = string.ascii_letters
-    numbers = string.digits
-    special_characters = string.punctuation
-
-    # Connect the characters together 
-    alphabet = letters + numbers + special_characters 
-
-    # Set a password length 
-    password_length = 18
-
-    # Now to generate the password
+class Password_Generator:
     password = ''
 
-    for i in range(password_length): 
-        password += ''.join(secrets.choice(alphabet))
+    def generate_pwd(self):
+        # All characters
+
+        letters = string.ascii_letters
+        numbers = string.digits
+        special_characters = string.punctuation
+
+        # Connect the characters together 
+        alphabet = letters + numbers + special_characters 
+        password_length = 18
+        for i in range(password_length): 
+            generated_password += ''.join(secrets.choice(alphabet))
+        
+        password = generated_password
+
+        return password
 
     # Password with Constraints
     # while True: 
@@ -25,5 +26,3 @@ class Password_Generator():
     #         password += ''.join(secrets.choice(alphabet))
 
     #     if (any(char in special_characters for char in password) and sum(char in numbers for char in password) >=2 ): break
-
-    print(password)
